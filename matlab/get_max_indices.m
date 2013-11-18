550874147c40603ad7b4fec71f90e6b884a3c9be
yeara <yeara.kozlov@gmail.com>
2013-11-18 19:59:39 +0100

    - Added two matlab functions to retrieve minima and maxima indices from PairedErtrema matrix
    - Updated documentation to reflect the change

---
+++ matlab/get_max_indices.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GET_MAX_INDICES
+%
+% Returns the indices of the local maxima from the 
+% results of filtered pairs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [ idx ] = get_max_indices( pairs )
+    idx = pairs(:,2);
+end
---
+++ matlab/get_min_indices.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% GET_MIN_INDICES
+%
+% Returns the indices of the local minima from the 
+% results of filtered pairs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [ idx ] = get_min_indices( pairs )
+    idx = pairs(:,1); 
+end
